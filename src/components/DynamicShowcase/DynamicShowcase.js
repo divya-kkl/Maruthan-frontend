88ffe2445be5d129103f5fc625adbf070fdc06fc
divya-kkl <divyakkl2002@gmail.com>
2026-07-28 11:12:39 +0530

    react-delete

--- src/components/DynamicShowcase/DynamicShowcase.js
+++ src/components/DynamicShowcase/DynamicShowcase.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import '../ProductShowcase/ProductShowcase.css';
 import { useNavigate } from 'react-router-dom';
 import QuickViewModal from '../QuickViewModal/QuickViewModal';
