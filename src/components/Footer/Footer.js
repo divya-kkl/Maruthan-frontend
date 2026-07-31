c45a9b1ddc918249ef8ec2c34e950decfae20a14
divya-kkl <divyakkl2002@gmail.com>
2026-07-31 15:45:59 +0530

    footer-logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,9 +25,9 @@ const Footer = ({ onNavigate }) => {
 
         {/* Column 1 */}
         <div className="footer-brand">
-          <Link to="/">
+          <a href="/">
             <img src={brand.logo} alt="Little RR" className="footer-logo" style={{ maxWidth: '110px', marginTop: '-20px', marginBottom: '10px' }} />
-          </Link>
+          </a>
           <p className="footer-description" style={{ fontSize: '14px', lineHeight: '1.5', color: '#fff', margin: 0 }}>
             {brand.description}
           </p>
